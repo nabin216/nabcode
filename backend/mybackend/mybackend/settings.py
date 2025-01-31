55b2e92dec733055495679a2d33d1849f59ec24a
Nabin <asadurzamannabin@gmail.com>
2025-01-31 19:42:42 +0530

    ergikernwgdhnj

--- backend/mybackend/mybackend/settings.py
+++ backend/mybackend/mybackend/settings.py
@@ -1,5 +1,3 @@
-
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,7 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
